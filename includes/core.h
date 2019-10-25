60ea22090b6ad9ded9044e79ec02f20c75014d04
Lucas <lucaspaulom@hotmail.com>
2019-10-25 17:22:59 -0300

    refactor: 💡 Improve code

--- includes/core.h
+++ includes/core.h
@@ -1,7 +1,34 @@
-#include "dinamico.h"
-#include "guloso.h"
+typedef struct {
+    int cost;
+    int pontuation;
+    double custoPerPontuation;
+} Island;
+
+// Dynamic functions:
 
 // #1)
-void initProgram (FILE *file);
+int max(int a, int b);
 // #2)
-void printAllInformations(int N, int M, int *costs, int *pontuations);
+void knapSack(int **K, int W, int wt[], int val[], int n, Island *islands);
+// #3)
+void initDynamicSoluction(Island *islands, int N, int M, int *costs, int *pontuations);
+
+
+// Greedy functions:
+
+// #1)
+void merge(Island *arr, int l, int m, int r);
+// #2)
+void mergeSort(Island *arr, int l, int r);
+// #3)
+void calcTripInformations(Island *islands, int budget, int numberOfIslands);
+// #4)
+void initGreedSoluction(Island *islands, int N, int M, int *costs, int *pontuations);
+
+
+// Utils functions:
+
+// #1) Fill island object with file informations
+void fillObject(Island *islands, int *costs, int *pontuations, int size);
+// #2) Read all informations and calc solutions
+void initProgram (FILE *file);
--- makefile
+++ makefile
@@ -1,16 +1,10 @@
-all: src/main.o src/dinamico.o src/guloso.o src/core.o
-	gcc -O3 src/main.o src/dinamico.o src/guloso.o src/core.o -o tp2
+all: src/main.o src/core.o
+	gcc -O3 src/main.o src/core.o -o tp2
 	make clean
 	clear
 
-dinamico.o: src/dinamico.c includes/dinamico.h
-	gcc -O3 -c src/dinamico.c
-
 core.o: src/core.c includes/core.h
 	gcc -O3 -c src/core.c
-
-guloso.o: src/guloso.c includes/guloso.h
-	gcc -O3 -c src/guloso.c
 
 main.o: src/main.c
 	gcc -O3 -c src/main.c
